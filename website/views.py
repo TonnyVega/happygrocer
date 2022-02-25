38f599c54c470facb2e8d80f9ed1d817334683a7
TonnyVega <tonycndirangu@gmail.com>
2022-02-25 18:38:50 +0300

    commit message

--- website/views.py
+++ website/views.py
@@ -7,7 +7,7 @@ import json
 views = Blueprint('views', __name__)
 
 
-@views.route('/home', methods=['GET','POST'])
+@views.route('/', methods=['GET','POST'])
 @login_required
 def home():
     return render_template('home.html', user=current_user)
