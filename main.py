266e4a76722de7d286ddb5e9d63e1bbf13f912ad
TonnyVega <tonycndirangu@gmail.com>
2022-02-28 17:32:43 +0300

    commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from website import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
 
 # this was my first attempt with blue prints and slight functioning inventory app, with a rough template.
